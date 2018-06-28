1c9fc05191aa8faded4c0559b9ea7d670d27ae78
Andrew <andrewtugume2@gmail.com>
2018-06-27 23:34:51 -0700

    worked on css

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("login-form");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
